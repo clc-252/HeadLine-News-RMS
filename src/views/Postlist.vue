925b1a23ae860998e4ab6009c84f0f1ba4abe459
蔡丽纯 <12345.qq.com>
2020-01-12 20:56:14 +0800

    08-完成文章列表组件的制作(使用Breadcrumb面包屑、table表格和ToolTip文字提示)

--- src/views/Postlist.vue
+++ src/views/Postlist.vue
@@ -1,13 +1,79 @@
 <template>
-  <div class="poetlist">文章列表</div>
+  <div class="poetlist">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>文章管理</el-breadcrumb-item>
+      <el-breadcrumb-item>文章列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- table表格 -->
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <!-- ToolTip文字提示 -->
+          <el-tooltip class="item" effect="dark" content="编辑" placement="top">
+            <el-button
+              type="primary"
+              @click="handleEdit(scope.$index, scope.row)"
+              icon="el-icon-edit"
+            ></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="分享" placement="top">
+            <el-button
+              type="success"
+              @click="handleShare(scope.$index, scope.row)"
+              icon="el-icon-share"
+            ></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="删除" placement="top">
+            <el-button
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+              icon="el-icon-delete"
+            ></el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.el-table {
+  margin-top: 20px;
+}
 </style>
